330aa6e0b6632f1151996295fc04fe920b769c56
venkatesh <venky@example.com>
2025-03-06 11:58:02 -0800

    apiPath made changes

--- src/vendorDashboard/data/apiPath.js
+++ src/vendorDashboard/data/apiPath.js
@@ -1,3 +1,4 @@
- export const API_URL = "http://localhost:4001"
+ //export const API_URL = "http://localhost:4001"
 
-//export const API_URL = "https://backend-nodejs-suby.onrender.com"
+// export const API_URL = "https://backend-nodejs-suby.onrender.com"
+export const API_URL = "https://backend-nodejs-hdw5.onrender.com"
